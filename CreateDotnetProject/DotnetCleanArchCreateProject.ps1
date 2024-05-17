a60d142ce0427da941702431cf57b7458667be50
Bhuvanesh Sahadhevan <bhuvaneshmib@gmail.com>
2024-05-17 22:31:51 +0530

    Correct the script

--- CreateDotnetProject/DotnetCleanArchCreateProject.ps1
+++ CreateDotnetProject/DotnetCleanArchCreateProject.ps1
@@ -12,38 +12,42 @@ if (Test-Path $projectsOutput) {
 dotnet new sln -n $solutionName -o $projectsOutput
 Set-Location $projectsOutput
 dotnet new classlib -o "$projectPath.Core"
+dotnet new classlib -o "$projectPath.Application"
 dotnet new classlib -o "$projectPath.Infrastructure"
-dotnet new classlib -o "$projectPath.Applications"
-dotnet new webapi -o "$projectPath.Api"
+dotnet new webapi -o "$projectPath.API"
 
 # Add projects to solution
 dotnet sln add "$projectPath.Core"
+dotnet sln add "$projectPath.Application"
 dotnet sln add "$projectPath.Infrastructure"
-dotnet sln add "$projectPath.Applications"
-dotnet sln add "$projectPath.Api"
+dotnet sln add "$projectPath.API"
 
 # Add reference to projects
 dotnet add "$projectPath.Infrastructure" reference "$projectPath.Core"
-dotnet add "$projectPath.Applications" reference "$projectPath.Infrastructure"
-dotnet add "$projectPath.Api" reference "$projectPath.Applications"
+dotnet add "$projectPath.API" reference "$projectPath.Application"
+dotnet add "$projectPath.Application" reference "$projectPath.Infrastructure"
+
+dotnet add "$projectPath.Application" reference "$projectPath.Core"
+dotnet add "$projectPath.Infrastructure" reference "$projectPath.Application"
+dotnet add "$projectPath.API" reference "$projectPath.Infrastructure"
 
 # Create test projects
 dotnet new xunit -o "$testPath.Core.Tests"
 dotnet new xunit -o "$testPath.Infrastructure.Tests"
-dotnet new xunit -o "$testPath.Applications.Tests"
+dotnet new xunit -o "$testPath.Application.Tests"
 dotnet new xunit -o "$testPath.FunctionalTests"
 
 # Add test projects to solution
 dotnet sln add "$testPath.Core.Tests"
+dotnet sln add "$testPath.Application.Tests"
 dotnet sln add "$testPath.Infrastructure.Tests"
-dotnet sln add "$testPath.Applications.Tests"
 dotnet sln add "$testPath.FunctionalTests"
 
 # Add reference to test projects
 dotnet add "$testPath.Core.Tests" reference "$projectPath.Core"
+dotnet add "$testPath.Application.Tests" reference "$projectPath.Application"
 dotnet add "$testPath.Infrastructure.Tests" reference "$projectPath.Infrastructure"
-dotnet add "$testPath.Applications.Tests" reference "$projectPath.Applications"
-dotnet add "$testPath.FunctionalTests" reference "$projectPath.Api"
+dotnet add "$testPath.FunctionalTests" reference "$projectPath.API"
 
 # Modify Project name in the README.md and Copy README.md to project root
 Set-Location ..
@@ -58,7 +62,8 @@ Set-Content $readmePath $readmeContent
 $vsCode = Get-Command code -ErrorAction SilentlyContinue
 if ($null -eq $vsCode) {
     Write-Host "Visual Studio Code is not installed. Please install it to open the project."
-} else {
+}
+else {
     # Open project in VS Code
     code .
 }
