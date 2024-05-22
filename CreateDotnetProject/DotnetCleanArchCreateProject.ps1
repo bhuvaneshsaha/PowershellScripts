a8310d82b41bfcf79babb5f6d6567972f91bbce4
Bhuvanesh Sahadhevan <bhuvaneshmib@gmail.com>
2024-05-22 11:07:58 +0530

    Change Core name to domain

--- CreateDotnetProject/DotnetCleanArchCreateProject.ps1
+++ CreateDotnetProject/DotnetCleanArchCreateProject.ps1
@@ -18,40 +18,36 @@ dotnet new gitignore -o $projectsOutput
 # Create solution and projects
 dotnet new sln -n $solutionName -o $projectsOutput
 Set-Location $projectsOutput
-dotnet new classlib -o "$projectPath.Core"
+dotnet new classlib -o "$projectPath.Domain"
 dotnet new classlib -o "$projectPath.Application"
 dotnet new classlib -o "$projectPath.Infrastructure"
 dotnet new webapi -o "$projectPath.API"
 
 # Add projects to solution
-dotnet sln add "$projectPath.Core"
+dotnet sln add "$projectPath.Domain"
 dotnet sln add "$projectPath.Application"
 dotnet sln add "$projectPath.Infrastructure"
 dotnet sln add "$projectPath.API"
 
 # Add reference to projects
-dotnet add "$projectPath.Infrastructure" reference "$projectPath.Core"
-dotnet add "$projectPath.API" reference "$projectPath.Application"
-dotnet add "$projectPath.Application" reference "$projectPath.Infrastructure"
-
-dotnet add "$projectPath.Application" reference "$projectPath.Core"
+dotnet add "$projectPath.Application" reference "$projectPath.Domain"
 dotnet add "$projectPath.Infrastructure" reference "$projectPath.Application"
 dotnet add "$projectPath.API" reference "$projectPath.Infrastructure"
 
 # Create test projects
-dotnet new xunit -o "$testPath.Core.Tests"
+dotnet new xunit -o "$testPath.Domain.Tests"
 dotnet new xunit -o "$testPath.Infrastructure.Tests"
 dotnet new xunit -o "$testPath.Application.Tests"
 dotnet new xunit -o "$testPath.FunctionalTests"
 
 # Add test projects to solution
-dotnet sln add "$testPath.Core.Tests"
+dotnet sln add "$testPath.Domain.Tests"
 dotnet sln add "$testPath.Application.Tests"
 dotnet sln add "$testPath.Infrastructure.Tests"
 dotnet sln add "$testPath.FunctionalTests"
 
 # Add reference to test projects
-dotnet add "$testPath.Core.Tests" reference "$projectPath.Core"
+dotnet add "$testPath.Domain.Tests" reference "$projectPath.Domain"
 dotnet add "$testPath.Application.Tests" reference "$projectPath.Application"
 dotnet add "$testPath.Infrastructure.Tests" reference "$projectPath.Infrastructure"
 dotnet add "$testPath.FunctionalTests" reference "$projectPath.API"
